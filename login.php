7c9cdef30c0a55be4cd59280fed6855cb4af1092
Zehra USTA <zehrusta1542@gmail.com>
2026-05-15 21:50:26 +0300

    100 inşallah

--- login.php
+++ login.php
@@ -2,22 +2,21 @@
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-// mailden öğrenci no çek
-$studentNo = explode("@", $email)[0];
-
-// kontrol
-if ($email == $studentNo . "@sakarya.edu.tr" && $password == $studentNo) {
-
-    header("Location: welcome.php?user=" . $studentNo);
-    exit();
-
-} else {
-
-    echo "<script>
-        alert('Hatalı giriş!');
-        window.location.href='login.html';
-    </script>";
-
+if($email == "b251210019@sakarya.edu.tr" && $password == "123456"){
+    header("refresh:3;url=index.html");
+    echo "
+    <h1 style='text-align:center; margin-top:100px; color:green;'>
+    Hoşgeldiniz $email
+    </h1>
+    <p style='text-align:center;'>3 saniye içinde ana sayfaya yönlendiriliyorsunuz...</p>
+    ";
 }
-
+else{
+    echo "
+    <h1 style='text-align:center; margin-top:100px; color:red;'>
+    Hatalı giriş!
+    </h1>
+    <a href='login.html'>Tekrar Dene</a>
+    ";
+}
 ?>
